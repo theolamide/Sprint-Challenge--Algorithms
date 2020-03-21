15c03af5125992f4943a7d22aace47bac2bfd0cd
theolamide <ajadidan@gmail.com>
2020-03-21 14:54:25 -0600

    MVPcomplete

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,26 +3,30 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+counted = 0
 
 
 def count_th(word):
-    counted = []
+    global counted
+    counted = 0
     th = "th"
 
     def num_of_times(word, th):
+        global counted
         into_array = list(word)
         if len(into_array) >= 2:
             to_check = into_array[0] + into_array[1]
             if th in to_check:
-                counted.append(1)
+                counted += 1
             sub_array = into_array[1:]
             return num_of_times(sub_array, th)
         else:
-            return 0
+            return counted
 
-    num_of_times(word, th)
-    collated_count = sum(counted)
-    return collated_count
+    counted = num_of_times(word, th)
+    print(counted)
+    return counted
 
 
-count_th("Thougth")
+count_th("thhtthht")
+
